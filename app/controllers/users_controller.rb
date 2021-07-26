87b756b83c30c9c0cd2e6957e35054bc817098b5
adebray <debray.arnaud@gmail.com>
2021-07-26 15:27:39 +0200

    CRUD implementation for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,38 @@ class UsersController < ApplicationController
     render :json => User.all
   end
 
-  def create
-    @user = User.new(name: params[:name])
+  def show
+    render :json => User.find(params[:id])
+  end
 
-    if @user.save
+  def create
+    user = User.new(name: params[:name])
+
+    user_save user
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    user.attributes = {
+      name: params[:name]
+    }
+
+    user_save user
+  end
+  
+  def destroy
+    if User.find(params[:id]).destroy
+      render status: :ok,
+          json: {
+            status: :ok
+          }
+      end
+  end
+
+  private
+  def user_save(user)
+    if user.save!
       render status: :created,
         json: {
           status: :created

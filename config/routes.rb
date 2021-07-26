687e6b14256f346d5c17f8a025d21b729b04049c
adebray <debray.arnaud@gmail.com>
2021-07-26 15:28:25 +0200

    a more precise routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :messages
-  end
+  get '/users/:id/messages', to: 'messages#by_recipient'
+  get '/thread/:id', to: 'threads#show'
+  get '/thread', to: 'threads#index'
+ 
+  resources :users 
   resources :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

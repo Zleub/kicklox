aedbf4ab896ce33e71e177f7640463cbe925024d
adebray <debray.arnaud@gmail.com>
2021-07-29 14:48:32 +0200

    route to get a thread by recipient

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/users/:id/messages', to: 'messages#by_recipient'
   get '/thread/:id', to: 'threads#show'
   get '/thread', to: 'threads#index'
+
+  get '/users/:user_id/thread/:id', to: 'threads#by_recipient'
  
   resources :users 
   resources :messages

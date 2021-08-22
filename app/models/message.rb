968591050ce27b3346b8f5a5119697d89a1440e2
adebray <debray.arnaud@gmail.com>
2021-08-22 16:25:20 +0200

    implementation of ActiveRecord::Callbacks

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,4 +7,9 @@ class Message < ApplicationRecord
     belongs_to :author, class_name: "User"
     belongs_to :recipient, class_name: "User"
     belongs_to :parent, class_name: "Message", optional: true, dependent: :destroy
+
+    before_destroy { |message| 
+        Message.where(parent_id: message.id).destroy_all
+        message.destroy
+    }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
     has_many :archived_messages, foreign_key: :author_id, dependent: :nullify
     has_many :archived_messages, foreign_key: :recipient_id, dependent: :nullify
+
+    before_destroy { |user| 
+        Message.where(recipient_id: user.id).update_all(recipient_id: nil)
+        Message.where(author_id: user.id).destroy_all
+    }
 end

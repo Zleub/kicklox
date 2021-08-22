f28cd1c6be8b945ea5f8064f8531b143e5e84092
adebray <debray.arnaud@gmail.com>
2021-08-22 17:21:03 +0200

    fix on message model and accordingly message model tests

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,7 +6,7 @@ class Message < ApplicationRecord
 
     belongs_to :author, class_name: "User"
     belongs_to :recipient, class_name: "User"
-    belongs_to :parent, class_name: "Message", optional: true, dependent: :destroy
+    belongs_to :parent, class_name: "Message", optional: true
 
     before_destroy { |message| 
         Message.where(parent_id: message.id).destroy_all
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,44 +1,51 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   test "Should save a message with a body, a public state, an author and a recipient" do
-    user = User.find_by(name: "Arnaud")
-    message = Message.new(body: "Hello World", public: true, author: user, recipient: user)
+    message = Message.new(body: "Hello World", public: true, author: users(:arnaud), recipient: users(:toto))
     assert message.save, "Failed to save a message with the basic parameters"
   end
 
   test "Should not save a message without a body" do
-    user = User.find_by(name: "Arnaud")
-    message = Message.new(public: true, author: user, recipient: user)
+    message = Message.new(public: true, author: users(:arnaud), recipient: users(:toto))
     assert_not message.save, "Saved a message without a body"
   end
 
   test "Should not save a message without a public state" do
-    user = User.find_by(name: "Arnaud")
-    message = Message.new(body: "Hello World", author: user, recipient: user)
+    message = Message.new(body: "Hello World", author: users(:arnaud), recipient: users(:toto))
     assert_not message.save, "Saved a message without a public state"
   end
 
   test "Should not save a message without a author" do
-    user = User.find_by(name: "Arnaud")
-    message = Message.new(body: "Hello World", public: true, recipient: user)
+    message = Message.new(body: "Hello World", public: true, recipient: users(:toto))
     assert_not message.save, "Saved a message without a author"
   end
 
   test "Should not save a message without a recipient" do
-    user = User.find_by(name: "Arnaud")
-    message = Message.new(body: "Hello World", public: true, author: user)
+    message = Message.new(body: "Hello World", public: true, author: users(:arnaud))
     assert_not message.save, "Saved a message without a recipient"
   end
   
   test "Should save a message with a parent" do
-    user = User.find_by(name: "Arnaud")
-    message = Message.find_by(body: "MyText")
-    message = Message.new(body: "Hello World", public: true, author: user, recipient: user, parent: message)
+    message = Message.new(body: "Hello World", public: true, author: users(:arnaud), recipient: users(:toto), parent: messages(:one))
     assert message.save, "Failed to save a message with a parent"
+  end
+
+  test "Should destroy a message without a parent" do
+    count = Message.count
+    assert messages(:one).destroy, "Failed to destroy a message without a parent"
+    assert_equal Message.count, count - 1, "Should have only deleted one message"
+  end
+
+  test "Should destroy a message with a parent" do
+    count = Message.count
+    assert messages(:seven).destroy, "Failed to destroy a message with a parent"
+    assert_equal Message.count, count - 1, "Should have only deleted one message"
+  end
+
+  test "Should destroy a message with a parent and a child" do
+    count = Message.count
+    assert messages(:four).destroy, "Failed to destroy a message with a parent"
+    assert_equal Message.count, count - 2, "Should have only deleted one message"
   end
 end

c02c18c445f1138dc8d219d483b5adea9bd4640d
adebray <debray.arnaud@gmail.com>
2021-08-23 13:57:14 +0200

    more tests for controllers

--- test/controllers/threads_controller_test.rb
+++ test/controllers/threads_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class ThreadsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should get Threads index" do
+    get threads_url
+
+    assert_response :success, "Response is not :success"
+    assert_equal @response.parsed_body.length, 2, "Response body length != 2"
+  end
+
+  test "Should get a thread by id" do
+    get threads_url
+    thread = @response.parsed_body[0]
+
+    get "/threads/#{thread}"
+
+    assert_response :success, "Response is not :success"
+    assert_instance_of Array, @response.parsed_body, "Response should have Array type" 
+    @response.parsed_body.each{ |msg| 
+      assert_not_nil msg, "Message should not be nil"
+    }
+  end
+  
+  test "Should not get a thread with a false id" do
+    get '/threads/-1'
+
+    assert_response :bad_request, "Response is not :bad_request"
+  end
+
+  test "Should get messages from a thread for a specific user" do
+    get threads_url
+    thread = @response.parsed_body[0]
+    toto = users(:toto)
+
+    get "/users/#{toto.id}/thread/#{thread}"
+
+    assert_response :ok, "Response is not :ok"
+    assert_instance_of Array, @response.parsed_body, "Response should have Array type" 
+    assert_equal @response.parsed_body.count, 2, "Response should be a 2 length array"
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,63 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get users_index_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get users_url
+
+    assert_response :success
+    assert_equal @response.parsed_body.count, 2, "Should respond with all users"    
+  end
+
+  test "should get a user by id" do
+    get user_url( users(:arnaud) )
+
+    assert_response :success
+    assert_equal @response.parsed_body["name"], "Arnaud", "Should respond with user Arnaud"    
+  end
+
+  test "should fail to get a user with false id" do
+    get user_url( -1 )
+
+    assert_response :bad_request
+  end
+
+  test "should create a user" do
+    post users_url, params: { name: "Arandel" }
+
+    assert_response :created
+  end
+
+  test "should not create a user without a name" do
+    post users_url, params: {}
+
+    assert_response :bad_request
+  end
+
+  test "should update a user" do
+    patch user_url(users(:arnaud)), params: { name: "Arno" }
+    
+    assert_response :success
+    assert_equal User.find(users(:arnaud).id).name, "Arno", "Should have changed the name of the user"
+  end
+
+  test "should not update a user with a false id" do
+    patch user_url(-1), params: { name: "Arno" }
+    
+    assert_response :bad_request
+  end
+
+  test "should delete a user" do
+    delete user_url(users(:arnaud))
+    
+    assert_response :success
+    assert_equal User.all.count, 1, "Should have deleted one user"
+  end
+
+  test "should not delete a user with a false id" do
+    delete user_url(users(:arnaud))
+    
+    assert_response :success
+    assert_equal User.all.count, 1, "Should have deleted one user"
+  end
 
 end
